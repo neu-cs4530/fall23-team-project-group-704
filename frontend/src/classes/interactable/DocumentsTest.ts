02482446010075daba220fbe15771aa0cd6a3a2a
Aminah Statham-Adams <97044463+aminahSA@users.noreply.github.com>
2023-11-06 10:12:05 -0500

    Initial setup/example tests

--- frontend/src/classes/interactable/DocumentsTest.ts
+++ frontend/src/classes/interactable/DocumentsTest.ts
@@ -1,1 +1,391 @@
+//got imports from tictactoeareatest, delete any not needed and add those needed
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import { mock, mockReset } from 'jest-mock-extended';
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { act } from 'react-dom/test-utils';
+import TicTacToeAreaController, {
+  TicTacToeCell,
+} from '../../../../classes/interactable/TicTacToeAreaController';
+import PlayerController from '../../../../classes/PlayerController';
+import TownController, * as TownControllerHooks from '../../../../classes/TownController';
+import TownControllerContext from '../../../../contexts/TownControllerContext';
+import {
+  GameArea,
+  GameResult,
+  GameStatus,
+  PlayerLocation,
+  TicTacToeGameState,
+} from '../../../../types/CoveyTownSocket';
+import PhaserGameArea from '../GameArea';
+import * as Leaderboard from '../Leaderboard';
+import TicTacToeAreaWrapper from './TicTacToeArea';
+import * as TicTacToeBoard from './TicTacToeBoard';
+
+const mockToast = jest.fn();
+jest.mock('@chakra-ui/react', () => {
+  const ui = jest.requireActual('@chakra-ui/react');
+  const mockUseToast = () => mockToast;
+  return {
+    ...ui,
+    useToast: mockUseToast,
+  };
+});
+
+//from tttactest
+const mockGameArea = mock<PhaserGameArea>();
+mockGameArea.getData.mockReturnValue('TicTacToe');
+jest.spyOn(TownControllerHooks, 'useInteractable').mockReturnValue(mockGameArea);
+const useInteractableAreaControllerSpy = jest.spyOn(
+  TownControllerHooks,
+  'useInteractableAreaController',
+);
+
+//from tttactest
+const leaderboardComponentSpy = jest.spyOn(Leaderboard, 'default');
+leaderboardComponentSpy.mockReturnValue(<div data-testid='leaderboard' />);
+
+//from tttactest
+const boardComponentSpy = jest.spyOn(TicTacToeBoard, 'default');
+boardComponentSpy.mockReturnValue(<div data-testid='board' />);
+
+const randomLocation = (): PlayerLocation => ({
+  moving: Math.random() < 0.5,
+  rotation: 'front',
+  x: Math.random() * 1000,
+  y: Math.random() * 1000,
+});
+
+//example mock from tttactest
+class MockTicTacToeAreaController extends TicTacToeAreaController {
+  makeMove = jest.fn();
+
+  joinGame = jest.fn();
+
+  mockBoard: TicTacToeCell[][] = [
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+  ];
+
+  mockIsPlayer = false;
+
+  mockIsOurTurn = false;
+
+  mockObservers: PlayerController[] = [];
+
+  mockMoveCount = 0;
+
+  mockWinner: PlayerController | undefined = undefined;
+
+  mockWhoseTurn: PlayerController | undefined = undefined;
+
+  mockStatus: GameStatus = 'WAITING_TO_START';
+
+  mockX: PlayerController | undefined = undefined;
+
+  mockO: PlayerController | undefined = undefined;
+
+  mockCurrentGame: GameArea<TicTacToeGameState> | undefined = undefined;
+
+  mockGamePiece: 'X' | 'O' = 'X';
+
+  mockIsActive = false;
+
+  mockHistory: GameResult[] = [];
+
+  public constructor() {
+    super(nanoid(), mock<GameArea<TicTacToeGameState>>(), mock<TownController>());
+  }
+
+  get board(): TicTacToeCell[][] {
+    throw new Error('Method should not be called within this component.');
+  }
+
+  get history(): GameResult[] {
+    return this.mockHistory;
+  }
+
+  get isOurTurn() {
+    return this.mockIsOurTurn;
+  }
+
+  get x(): PlayerController | undefined {
+    return this.mockX;
+  }
+
+  get o(): PlayerController | undefined {
+    return this.mockO;
+  }
+
+  get observers(): PlayerController[] {
+    return this.mockObservers;
+  }
+
+  get moveCount(): number {
+    return this.mockMoveCount;
+  }
+
+  get winner(): PlayerController | undefined {
+    return this.mockWinner;
+  }
+
+  get whoseTurn(): PlayerController | undefined {
+    return this.mockWhoseTurn;
+  }
+
+  get status(): GameStatus {
+    return this.mockStatus;
+  }
+
+  get isPlayer() {
+    return this.mockIsPlayer;
+  }
+
+  get gamePiece(): 'X' | 'O' {
+    return this.mockGamePiece;
+  }
+
+  public isActive(): boolean {
+    return this.mockIsActive;
+  }
+
+  public mockReset() {
+    this.mockBoard = [
+      ['X', 'O', undefined],
+      [undefined, 'X', undefined],
+      [undefined, undefined, 'O'],
+    ];
+    this.makeMove.mockReset();
+  }
+}
+
+//my tests
+describe('[T1] documents', () => {
+
+beforeAll(() => {})
+afterAll(() => {})
+
+  it('Does something', () => {
+     
+    });
+}])
+
+//example test from tictactoearea
+describe('[T2] TicTacToeArea', () => {
+  // Spy on console.error and intercept react key warnings to fail test
+  let consoleErrorSpy: jest.SpyInstance<void, [message?: any, ...optionalParms: any[]]>;
+  beforeAll(() => {
+    // Spy on console.error and intercept react key warnings to fail test
+    consoleErrorSpy = jest.spyOn(global.console, 'error');
+    consoleErrorSpy.mockImplementation((message?, ...optionalParams) => {
+      const stringMessage = message as string;
+      if (stringMessage.includes && stringMessage.includes('children with the same key,')) {
+        throw new Error(stringMessage.replace('%s', optionalParams[0]));
+      } else if (stringMessage.includes && stringMessage.includes('warning-keys')) {
+        throw new Error(stringMessage.replace('%s', optionalParams[0]));
+      }
+      // eslint-disable-next-line no-console -- we are wrapping the console with a spy to find react warnings
+      console.warn(message, ...optionalParams);
+    });
+  });
+  afterAll(() => {
+    consoleErrorSpy.mockRestore();
+  });
+
+  let ourPlayer: PlayerController;
+  const townController = mock<TownController>();
+  Object.defineProperty(townController, 'ourPlayer', { get: () => ourPlayer });
+  let gameAreaController = new MockTicTacToeAreaController();
+  let joinGameResolve: () => void;
+  let joinGameReject: (err: Error) => void;
+
+  function renderTicTacToeArea() {
+    return render(
+      <ChakraProvider>
+        <TownControllerContext.Provider value={townController}>
+          <TicTacToeAreaWrapper />
+        </TownControllerContext.Provider>
+      </ChakraProvider>,
+    );
+  }
+  beforeEach(() => {
+    ourPlayer = new PlayerController('player x', 'player x', randomLocation());
+    mockGameArea.name = nanoid();
+    mockReset(townController);
+    gameAreaController.mockReset();
+    useInteractableAreaControllerSpy.mockReturnValue(gameAreaController);
+    leaderboardComponentSpy.mockClear();
+    mockToast.mockClear();
+    gameAreaController.joinGame.mockReset();
+    gameAreaController.makeMove.mockReset();
+
+    gameAreaController.joinGame.mockImplementation(
+      () =>
+        new Promise<void>((resolve, reject) => {
+          joinGameResolve = resolve;
+          joinGameReject = reject;
+        }),
+    );
+  });
+  describe('[T2.1] Game update listeners', () => {
+    it('Registers exactly two listeners when mounted: one for gameUpdated and one for gameEnd', () => {
+      const addListenerSpy = jest.spyOn(gameAreaController, 'addListener');
+      addListenerSpy.mockClear();
+
+      renderTicTacToeArea();
+      expect(addListenerSpy).toBeCalledTimes(2);
+      expect(addListenerSpy).toHaveBeenCalledWith('gameUpdated', expect.any(Function));
+      expect(addListenerSpy).toHaveBeenCalledWith('gameEnd', expect.any(Function));
+    });
+    it('Does not register listeners on every render', () => {
+      const removeListenerSpy = jest.spyOn(gameAreaController, 'removeListener');
+      const addListenerSpy = jest.spyOn(gameAreaController, 'addListener');
+      addListenerSpy.mockClear();
+      removeListenerSpy.mockClear();
+      const renderData = renderTicTacToeArea();
+      expect(addListenerSpy).toBeCalledTimes(2);
+      addListenerSpy.mockClear();
+
+      renderData.rerender(
+        <ChakraProvider>
+          <TownControllerContext.Provider value={townController}>
+            <TicTacToeAreaWrapper />
+          </TownControllerContext.Provider>
+        </ChakraProvider>,
+      );
+
+      expect(addListenerSpy).not.toBeCalled();
+      expect(removeListenerSpy).not.toBeCalled();
+    });
+    it('Removes the listeners when the component is unmounted', () => {
+      const removeListenerSpy = jest.spyOn(gameAreaController, 'removeListener');
+      const addListenerSpy = jest.spyOn(gameAreaController, 'addListener');
+      addListenerSpy.mockClear();
+      removeListenerSpy.mockClear();
+      const renderData = renderTicTacToeArea();
+      expect(addListenerSpy).toBeCalledTimes(2);
+      const addedListeners = addListenerSpy.mock.calls;
+      const addedGameUpdateListener = addedListeners.find(call => call[0] === 'gameUpdated');
+      const addedGameEndedListener = addedListeners.find(call => call[0] === 'gameEnd');
+      expect(addedGameEndedListener).toBeDefined();
+      expect(addedGameUpdateListener).toBeDefined();
+      renderData.unmount();
+      expect(removeListenerSpy).toBeCalledTimes(2);
+      const removedListeners = removeListenerSpy.mock.calls;
+      const removedGameUpdateListener = removedListeners.find(call => call[0] === 'gameUpdated');
+      const removedGameEndedListener = removedListeners.find(call => call[0] === 'gameEnd');
+      expect(removedGameUpdateListener).toEqual(addedGameUpdateListener);
+      expect(removedGameEndedListener).toEqual(addedGameEndedListener);
+    });
+    it('Creates new listeners if the gameAreaController changes', () => {
+      const removeListenerSpy = jest.spyOn(gameAreaController, 'removeListener');
+      const addListenerSpy = jest.spyOn(gameAreaController, 'addListener');
+      addListenerSpy.mockClear();
+      removeListenerSpy.mockClear();
+      const renderData = renderTicTacToeArea();
+      expect(addListenerSpy).toBeCalledTimes(2);
+
+      gameAreaController = new MockTicTacToeAreaController();
+      const removeListenerSpy2 = jest.spyOn(gameAreaController, 'removeListener');
+      const addListenerSpy2 = jest.spyOn(gameAreaController, 'addListener');
+
+      useInteractableAreaControllerSpy.mockReturnValue(gameAreaController);
+      renderData.rerender(
+        <ChakraProvider>
+          <TownControllerContext.Provider value={townController}>
+            <TicTacToeAreaWrapper />
+          </TownControllerContext.Provider>
+        </ChakraProvider>,
+      );
+      expect(removeListenerSpy).toBeCalledTimes(2);
+
+      expect(addListenerSpy2).toBeCalledTimes(2);
+      expect(removeListenerSpy2).not.toBeCalled();
+    });
+  });
+  describe('[T2.2] Rendering the leaderboard', () => {
+    it('Renders the leaderboard with the history when the component is mounted', () => {
+      gameAreaController.mockHistory = [
+        {
+          gameID: nanoid(),
+          scores: {
+            [nanoid()]: 1,
+            [nanoid()]: 0,
+          },
+        },
+      ];
+      renderTicTacToeArea();
+      expect(leaderboardComponentSpy).toHaveBeenCalledWith(
+        {
+          results: gameAreaController.mockHistory,
+        },
+        {},
+      );
+    });
+    it('Renders the leaderboard with the history when the game is updated', () => {
+      gameAreaController.mockHistory = [
+        {
+          gameID: nanoid(),
+          scores: {
+            [nanoid()]: 1,
+            [nanoid()]: 0,
+          },
+        },
+      ];
+      renderTicTacToeArea();
+      expect(leaderboardComponentSpy).toHaveBeenCalledWith(
+        {
+          results: gameAreaController.mockHistory,
+        },
+        {},
+      );
+
+      gameAreaController.mockHistory = [
+        {
+          gameID: nanoid(),
+          scores: {
+            [nanoid()]: 1,
+            [nanoid()]: 1,
+          },
+        },
+      ];
+      act(() => {
+        gameAreaController.emit('gameUpdated');
+      });
+      expect(leaderboardComponentSpy).toHaveBeenCalledWith(
+        {
+          results: gameAreaController.mockHistory,
+        },
+        {},
+      );
+    });
+  });
+  describe('[T2.3] Join game button', () => {
+    it('Is not shown when the player is in a not-yet-started game', () => {
+      gameAreaController.mockStatus = 'WAITING_TO_START';
+      gameAreaController.mockX = ourPlayer;
+      gameAreaController.mockIsPlayer = true;
+      renderTicTacToeArea();
+      expect(screen.queryByText('Join New Game')).not.toBeInTheDocument();
+    });
+    it('Is not shown if the game is in progress', () => {
+      gameAreaController.mockStatus = 'IN_PROGRESS';
+      gameAreaController.mockX = new PlayerController('player X', 'player X', randomLocation());
+      gameAreaController.mockO = new PlayerController('player O', 'player O', randomLocation());
+      gameAreaController.mockIsPlayer = false;
+      renderTicTacToeArea();
+      expect(screen.queryByText('Join New Game')).not.toBeInTheDocument();
+    });
+    it('Is enabled when the player is not in a game and the game is not in progress', () => {
+      gameAreaController.mockStatus = 'WAITING_TO_START';
+      gameAreaController.mockX = undefined;
+      gameAreaController.mockO = new PlayerController('player O', 'player O', randomLocation());
+      gameAreaController.mockIsPlayer = false;
+      renderTicTacToeArea();
+      expect(screen.queryByText('Join New Game')).toBeInTheDocument();
+    });
+});
 

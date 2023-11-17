95b7309889a505ccd9619642109e18fdc682951d
rosenmand <146114868+rosenmand@users.noreply.github.com>
2023-11-17 14:30:19 -0500

    Update Document.tsx

--- frontend/src/components/Town/interactables/TicTacToe/Document.tsx
+++ frontend/src/components/Town/interactables/TicTacToe/Document.tsx
@@ -1,76 +1,41 @@
-
-
-export default function Document({ doc }: { doc: ICDocArea }): JSX.Element {
-
-var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-tinymce.init({
-  selector: 'textarea#full-featured',
-  plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export',
-  tinydrive_token_provider: 'URL_TO_YOUR_TOKEN_PROVIDER',
-  tinydrive_dropbox_app_key: 'YOUR_DROPBOX_APP_KEY',
-  tinydrive_google_drive_key: 'YOUR_GOOGLE_DRIVE_KEY',
-  tinydrive_google_drive_client_id: 'YOUR_GOOGLE_DRIVE_CLIENT_ID',
-  mobile: {
-    plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker textpattern noneditable help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable'
-  },
-  menu: {
-    tc: {
-      title: 'Comments',
-      items: 'addcomment showcomments deleteallconversations'
-    }
-  },
-  menubar: 'file edit view insert format tools table tc help',
-  toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
-  autosave_ask_before_unload: true,
-  autosave_interval: '30s',
-  autosave_prefix: '{path}{query}-{id}-',
-  autosave_restore_when_empty: false,
-  autosave_retention: '2m',
-  image_advtab: true,
-  link_list: [
-    { title: 'My page 1', value: 'https://www.tiny.cloud' },
-    { title: 'My page 2', value: 'http://www.moxiecode.com' }
-  ],
-  image_list: [
-    { title: 'My page 1', value: 'https://www.tiny.cloud' },
-    { title: 'My page 2', value: 'http://www.moxiecode.com' }
-  ],
-  image_class_list: [
-    { title: 'None', value: '' },
-    { title: 'Some class', value: 'class-name' }
-  ],
-  importcss_append: true,
-  templates: [
-        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
-    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
-    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
-  ],
-  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-  height: 600,
-  image_caption: true,
-  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-  noneditable_noneditable_class: 'mceNonEditable',
-  toolbar_mode: 'sliding',
-  spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
-  tinycomments_mode: 'embedded',
-  content_style: '.mymention{ color: gray; }',
-  contextmenu: 'link image imagetools table configurepermanentpen',
-  a11y_advanced_options: true,
-  skin: useDarkMode ? 'oxide-dark' : 'oxide',
-  content_css: useDarkMode ? 'dark' : 'default',
-  /*
-  The following settings require more configuration than shown here.
-  For information on configuring the mentions plugin, see:
-  https://www.tiny.cloud/docs/plugins/premium/mentions/.
-  */
-  mentions_selector: '.mymention',
-  mentions_fetch: mentions_fetch,
-  mentions_menu_hover: mentions_menu_hover,
-  mentions_menu_complete: mentions_menu_complete,
-  mentions_select: mentions_select,
-  mentions_item_type: 'profile'
-});
-
+// DocumentViewer.tsx
+import React, { useEffect } from 'react';
+import TinyMCE from 'tinymce';
+import 'tinymce/themes/silver';
+import DocumentArea from './DocumentArea';
+import { ICDocDocument } from '../../../../types/CoveyTownSocket';
+interface DocumentViewerProps {
+  documentContent: string;
 }
+
+const Document: React.FC<ICDocDocument> = ({ currentDocument }) => {
+  useEffect(() => {
+    // Initialize TinyMCE with the provided document content
+    TinyMCE.init({
+      selector: '#documentEditor',
+      height: 500,
+      menubar: false,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount'
+      ],
+      toolbar: 'undo redo | formatselect | ' +
+               'bold italic backcolor | alignleft aligncenter ' +
+               'alignright alignjustify | bullist numlist outdent indent | ' +
+               'removeformat | help'
+    });
+
+    // Set the document content in the TinyMCE editor
+    TinyMCE.activeEditor.setContent(currentDocument.content);
+
+    // Clean up TinyMCE when the component unmounts
+    return () => {
+      TinyMCE.activeEditor.destroy();
+    };
+  }, [currentDocument.content]);
+
+  return <div id="documentEditor"></div>;
+};
+
+export default Document;

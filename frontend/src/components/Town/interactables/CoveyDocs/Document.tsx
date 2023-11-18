ffe3cd798ff08b406cef021f6247b8d02e8a7423
danielrosenman <danielr9187@gmail.com>
2023-11-18 18:31:49 -0500

    implemented the document component with tiny mce

---
+++ frontend/src/components/Town/interactables/CoveyDocs/Document.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { ICDocDocument } from '../../../../types/CoveyTownSocket';
+import CovDocsAreaController from '../../../../classes/interactable/CovDocsAreaController';
+export default function Document({
+  document,
+  controller,
+}: {
+  document: ICDocDocument;
+  controller: CovDocsAreaController;
+}): JSX.Element {
+  const editorRef = useRef<any>(null);
+  editorRef.current.setContent(document.content);
+
+  useEffect(() => {
+    const interval = setInterval(() => controller.writeToDoc(editorRef.current.getContent()), 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [controller]);
+
+  return (
+    <>
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue='<p>This is the initial content of the editor.</p>'
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+          ],
+          toolbar:
+            'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+    </>
+  );
+}

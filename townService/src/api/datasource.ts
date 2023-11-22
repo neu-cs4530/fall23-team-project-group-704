ad4df6be65d5312e354b62acc170aa8c832188a2
ise <okhiria.b@northeastern.edu>
2023-11-22 10:40:18 -0500

    created migration folder

--- townService/src/api/datasource.ts
+++ townService/src/api/datasource.ts
@@ -1,4 +1,7 @@
 import { DataSource } from 'typeorm';
+import Document from './document';
+import DocumentDirectory from './documentdirectory';
+import User from './user';
 
 const appDataSource: DataSource = new DataSource({
   type: 'postgres',
@@ -7,15 +10,7 @@ const appDataSource: DataSource = new DataSource({
   username: 'coveydocs_user',
   password: 'BnMjyOzetiuOg8VECO3gWl5KeYp5w4iZ',
   database: 'coveydocs',
+  entities: [Document, DocumentDirectory, User],
 });
 
 export default appDataSource;
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-  })
-  .catch(err => {
-    console.error('Error during Data Source initialization', err);
-  });

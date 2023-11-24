4a88d14b11686a06a70845df54d404ec76a2268f
Joseph Gu <josephgu2003@gmail.com>
2023-11-24 15:11:34 -0500

    work on sharing docs in backend
    rebase

--- townService/src/town/CDocUserDataMap.ts
+++ townService/src/town/CDocUserDataMap.ts
@@ -1,8 +1,16 @@
-import { CDocDocID, CDocUserID, ICDocUserDataMap } from '../types/CoveyTownSocket';
+import {
+  CDocDocID,
+  CDocUserID,
+  ExtendedPermissionType,
+  ICDocUserDataMap,
+  PermissionType,
+} from '../types/CoveyTownSocket';
 
 interface CDocUserData {
   activeDoc: CDocDocID | undefined;
   ownedDocs: CDocDocID[];
+  sharedDocsEdit: CDocDocID[];
+  sharedDocsView: CDocDocID[];
 }
 export class CDocUserDataMap implements ICDocUserDataMap {
   public isTrackingDoc(docid: string): boolean {
@@ -11,10 +19,28 @@ export class CDocUserDataMap implements ICDocUserDataMap {
     return this._docMap.find(entry => entry[1].activeDoc === docid) !== undefined;
   }
 
+  public isTrackingUser(userID: CDocUserID): boolean {
+    return this._docMap.find(entry => entry[0] === userID) !== undefined;
+  }
+
   private _docMap: [CDocUserID, CDocUserData][];
 
   constructor() {
     this._docMap = [];
+  }
+
+  public getSharedDocs(userID: string, permissionType: PermissionType): CDocDocID[] {
+    const foundEntry = this._docMap.find(entry => entry[0] === userID);
+
+    if (foundEntry) {
+      if (permissionType === 'EDIT') {
+        return foundEntry[1].sharedDocsEdit;
+      }
+      if (permissionType === 'VIEW') {
+        return foundEntry[1].sharedDocsView;
+      }
+    }
+    throw new Error('getSharedDocs failed');
   }
 
   public hasActiveDoc(userid: CDocUserID): boolean {
@@ -36,38 +62,85 @@ export class CDocUserDataMap implements ICDocUserDataMap {
   public setActiveDoc(userid: CDocUserID, docid: CDocDocID | undefined) {
     const foundEntry = this._docMap.find(entry => entry[0] === userid);
 
-    if (foundEntry) this._setUserData(userid, docid, foundEntry[1].ownedDocs);
-    else this._setUserData(userid, docid, []);
+    if (foundEntry)
+      this._setUserData(
+        userid,
+        docid,
+        foundEntry[1].ownedDocs,
+        foundEntry[1].sharedDocsEdit,
+        foundEntry[1].sharedDocsView,
+      );
+    else this._setUserData(userid, docid, [], [], []);
   }
 
   public setOwnedDocs(userid: CDocUserID, docs: CDocDocID[]) {
     const foundEntry = this._docMap.find(entry => entry[0] === userid);
 
-    if (foundEntry) this._setUserData(userid, foundEntry[1].activeDoc, docs);
-    else this._setUserData(userid, undefined, docs);
+    if (foundEntry)
+      this._setUserData(
+        userid,
+        foundEntry[1].activeDoc,
+        docs,
+        foundEntry[1].sharedDocsEdit,
+        foundEntry[1].sharedDocsView,
+      );
+    else this._setUserData(userid, undefined, docs, [], []);
   }
 
   public getOwnedDocs(userid: CDocUserID): CDocDocID[] {
     const foundEntry = this._docMap.find(entry => entry[0] === userid);
 
-    if (foundEntry && foundEntry[1].activeDoc) return foundEntry[1].ownedDocs;
-    throw new Error('User has no active doc');
+    if (foundEntry) return foundEntry[1].ownedDocs;
+    throw new Error('User not found');
   }
 
   public getOwnedDocsOrDefault(userid: CDocUserID): CDocDocID[] {
     const foundEntry = this._docMap.find(entry => entry[0] === userid);
 
-    if (foundEntry && foundEntry[1].activeDoc) return foundEntry[1].ownedDocs;
+    if (foundEntry) return foundEntry[1].ownedDocs;
     return [];
+  }
+
+  public shareWith(docID: CDocDocID, userID: CDocUserID, permissionType: ExtendedPermissionType) {
+    const foundEntry = this._docMap.find(entry => entry[0] === userID);
+
+    if (foundEntry) {
+      if (permissionType === 'REMOVE') {
+        this._setUserData(
+          userID,
+          foundEntry[1].activeDoc,
+          foundEntry[1].ownedDocs,
+          foundEntry[1].sharedDocsEdit.filter(user => user !== userID),
+          foundEntry[1].sharedDocsView.filter(user => user !== userID),
+        );
+      } else {
+        this._setUserData(
+          userID,
+          foundEntry[1].activeDoc,
+          foundEntry[1].ownedDocs,
+          foundEntry[1].sharedDocsEdit.concat(permissionType === 'EDIT' ? [docID] : []),
+          foundEntry[1].sharedDocsView.concat(permissionType === 'VIEW' ? [docID] : []),
+        );
+      }
+    }
+    throw new Error('User not found');
   }
 
   private _setUserData(
     userid: CDocUserID,
     activeDoc: CDocDocID | undefined,
     ownedDocs: CDocDocID[],
+    sharedEdit: CDocDocID[],
+    sharedView: CDocDocID[],
   ) {
     this._docMap.filter(entry => entry[0] !== userid);
-    this._docMap.push([userid, { activeDoc, ownedDocs }]);
+    const newData: CDocUserData = {
+      activeDoc,
+      ownedDocs,
+      sharedDocsEdit: sharedEdit,
+      sharedDocsView: sharedView,
+    };
+    this._docMap.push([userid, newData]);
   }
 }
 

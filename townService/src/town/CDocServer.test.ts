bf23de6de8ca4bdc04b219cc45719ed5720ddda4
Aminah Statham-Adams <97044463+aminahSA@users.noreply.github.com>
2023-11-27 21:32:38 -0500

    create test file for cdocserver

---
+++ townService/src/town/CDocServer.test.ts
@@ -0,0 +1,68 @@
+import { mock, mockClear } from 'jest-mock-extended';
+import { nanoid } from 'nanoid';
+import Player from '../lib/Player';
+import { getLastEmittedEvent } from '../TestUtils';
+import { TownEmitter } from '../types/CoveyTownSocket';
+import CDocServer from './CDocServer';
+import ConversationArea from './ConversationArea';
+
+describe('CDocServer', () => {
+  // initialize any needed constants
+
+  // do a beforeeach if necessary
+
+  describe('addSharedWithListener', () => {
+    it('does something', () => {});
+  });
+
+  describe('removeSharedWithListener', () => {
+    it('does something', () => {});
+  });
+  describe('removeSharedWithListener', () => {
+    it('does something', () => {});
+  });
+
+  describe('getSharedWith', () => {
+    it('does something', () => {});
+  });
+
+  describe('shareDocumentWith', () => {
+    it('does something', () => {});
+  });
+
+  describe('validateUser', () => {
+    it('does something', () => {});
+  });
+
+  describe('addDocumentEditedListener', () => {
+    it('does something', () => {});
+  });
+
+  describe('removeDocumentEditedListener', () => {
+    it('does something', () => {});
+  });
+
+  describe('getInstance', () => {
+    it('does something', () => {});
+  });
+
+  describe('createNewDoc', () => {
+    it('does something', () => {});
+  });
+
+  describe('createNewUser', () => {
+    it('does something', () => {});
+  });
+
+  describe('getOwnedDocs', () => {
+    it('does something', () => {});
+  });
+  describe('writeToDoc', () => {
+    it('does something', () => {});
+  });
+
+  describe('getDoc', () => {
+    it('does something', () => {});
+  });
+  
+});

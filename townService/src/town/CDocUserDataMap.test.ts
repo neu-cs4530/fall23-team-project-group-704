524ed732ad33b012b156a1c8d5b3f1a887b48355
ise <okhiria.b@northeastern.edu>
2023-11-28 21:27:19 -0500

    testing changes

---
+++ townService/src/town/CDocUserDataMap.test.ts
@@ -0,0 +1,97 @@
+import { CDocUserDataMap } from './CDocUserDataMap';
+
+describe('CDocUserDataMap', () => {
+  let cDocUserDataMap: CDocUserDataMap;
+
+  beforeEach(() => {
+    cDocUserDataMap = new CDocUserDataMap();
+  });
+
+  test('isTrackingDoc should return true when the document is being tracked', () => {
+    const docId = 'yourDocId';
+    cDocUserDataMap.setActiveDoc('userId1', docId);
+    expect(cDocUserDataMap.isTrackingDoc(docId)).toBe(true);
+  });
+
+  test('isTrackingDoc should return false when the document is not being tracked', () => {
+    const docId = 'yourDocId';
+    expect(cDocUserDataMap.isTrackingDoc(docId)).toBe(false);
+  });
+
+  test('isTrackingDoc should throw an error when given a null string', () => {
+    expect(() => cDocUserDataMap.isTrackingDoc('')).toThrow('Given null string in isTrackingDoc');
+  });
+
+  test('hasActiveDoc should return true when the user has an active document', () => {
+    const userId = 'userId1';
+    cDocUserDataMap.setActiveDoc(userId, 'docId');
+    expect(cDocUserDataMap.hasActiveDoc(userId)).toBe(true);
+  });
+
+  test('hasActiveDoc should return false when the user does not have an active document', () => {
+    const userId = 'userId1';
+    expect(cDocUserDataMap.hasActiveDoc(userId)).toBe(false);
+  });
+
+  test('hasActiveDoc should throw an error when given a null string', () => {
+    expect(() => cDocUserDataMap.hasActiveDoc('')).toThrow('Given null string in hasActiveDoc');
+  });
+
+  test('getActiveDoc should return the active document for a given user', () => {
+    const userId = 'userId1';
+    const activeDocId = 'docId';
+    cDocUserDataMap.setActiveDoc(userId, activeDocId);
+    expect(cDocUserDataMap.getActiveDoc(userId)).toBe(activeDocId);
+  });
+
+  test('getActiveDoc should throw an error when the user has no active document', () => {
+    const userId = 'userId1';
+    expect(() => cDocUserDataMap.getActiveDoc(userId)).toThrow('User has no active doc');
+  });
+
+  test('closeActiveDoc should set the active document to undefined for a given user', () => {
+    const userId = 'userId1';
+    cDocUserDataMap.setActiveDoc(userId, 'docId');
+    cDocUserDataMap.closeActiveDoc(userId);
+    expect(cDocUserDataMap.getActiveDoc(userId)).toBeUndefined();
+  });
+
+  test('setActiveDoc should set the active document for a given user', () => {
+    const userId = 'userId1';
+    const docId = 'docId';
+    cDocUserDataMap.setActiveDoc(userId, docId);
+    expect(cDocUserDataMap.getActiveDoc(userId)).toBe(docId);
+  });
+
+  test('setOwnedDocs should set the owned documents for a given user', () => {
+    const userId = 'userId1';
+    const docs = ['docId1', 'docId2'];
+    cDocUserDataMap.setOwnedDocs(userId, docs);
+    expect(cDocUserDataMap.getOwnedDocs(userId)).toEqual(docs);
+  });
+
+  test('getOwnedDocs should return the owned documents for a given user', () => {
+    const userId = 'userId1';
+    const docs = ['docId1', 'docId2'];
+    cDocUserDataMap.setOwnedDocs(userId, docs);
+    expect(cDocUserDataMap.getOwnedDocs(userId)).toEqual(docs);
+  });
+
+  test('getOwnedDocs should throw an error when the user has no active document', () => {
+    const userId = 'userId1';
+    expect(() => cDocUserDataMap.getOwnedDocs(userId)).toThrow('User has no active doc');
+  });
+
+  test('getOwnedDocsOrDefault should return the owned documents for a given user or an empty array', () => {
+    const userId = 'userId1';
+    const docs = ['docId1', 'docId2'];
+    cDocUserDataMap.setOwnedDocs(userId, docs);
+    expect(cDocUserDataMap.getOwnedDocsOrDefault(userId)).toEqual(docs);
+    expect(cDocUserDataMap.getOwnedDocsOrDefault('nonexistentUserId')).toEqual([]);
+  });
+
+  test('getOwnedDocsOrDefault should return an empty array when the user does not exist', () => {
+    const userId = 'nonexistentUserId';
+    expect(cDocUserDataMap.getOwnedDocsOrDefault(userId)).toEqual([]);
+  });
+});
